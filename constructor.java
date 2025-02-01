b9e28255696b55c353ca5fee6eada46c110ea953
harsha <harshaa.bsc23@rvu.edu.in>
2025-02-01 14:16:42 +0530

    update

---
+++ constructor.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Student {
+    String name;
+
+    // Constructor
+    Student(String n) {
+        name = n;
+    }
+
+    void display() {
+        System.out.println("Student Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Alice");
+        s1.display();
+    }
+}
