68ff85c533a5ebc03916919d86294d338e949793
harsha <harshaa.bsc23@rvu.edu.in>
2025-03-11 22:08:04 +0530

    executable packages

---
+++ TestPackage.java
@@ -0,0 +1,8 @@
+import mypackage.MyClass; // Import the package
+
+public class TestPackage {
+public static void main(String[] args) {
+MyClass obj = new MyClass(); // Create an object of MyClass
+obj.displayMessage(); // Call the method
+}
+}
