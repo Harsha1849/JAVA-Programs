464fb694506bbcb68f749777555ebc4cf6f93c66
harsha <harshaa.bsc23@rvu.edu.in>
2025-03-15 13:35:58 +0530

    practice

--- prac.java
+++ prac.java
@@ -1,16 +1,38 @@
-class Person{
-    String name;
-    int age;
+//class and object
 
-    void display(){
-        System.out.println("Name: " + name + ", Age: "+age);
+class Student{
+    String name;
+    int USN;
+
+    void display()
+    {
+        System.out.println("Name:" + name + ", USN:" + USN);
     }
 
-    public static void main(String[] args)
+    public static void main(String[]args)
     {
-        Person myperson=new Person();
-        myperson.name="nigga";
-        myperson.age=20;
-        myperson.display();
+        Student stu=new Student();
+        stu.name= "ABC";
+        stu.USN= 55;
+        stu.display();
+    }
+}
+
+//methods
+
+class Meth{
+    int add(int a, int b){
+        return a+b;
+    }
+
+    static int multiply(int a, int b){
+        return a*b;
+    }
+
+    public static void main(String[]args)
+    {
+        Meth me=new Meth();
+        System.out.println("Sum = "+me.add(4,5));
+        System.out.println("Product = "+ Meth.multiply(4, 5));
     }
 }
